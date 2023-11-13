05e03081fc7ac852c90b4511a756c401844cb218
Cam Spiers <cam.spiers@aviatnet.com>
2023-11-13 22:22:40 +1300

    Check if the issue is setting paths

--- lua/rocks.lua
+++ lua/rocks.lua
@@ -2,9 +2,10 @@ local luarocks = require("rocks.luarocks")
 
 return {
 	setup = function(opts)
-		package.path = package.path .. ";" .. luarocks.paths.share
-		package.cpath = package.cpath .. ";" .. luarocks.paths.lib
-
+		pcall(function()
+			package.path = package.path .. ";" .. luarocks.paths.share
+			package.cpath = package.cpath .. ";" .. luarocks.paths.lib
+		end)
 		if opts.rocks then
 			luarocks.ensure_rocks(opts.rocks)
 		end
